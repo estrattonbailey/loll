a3118f1b52ebb0524245d81efd297ed1f7bd35ee
estrattonbailey <ericstrattonbailey@gmail.com>
2018-01-09 09:12:05 -0500

    Update href dep in app

--- packages/loll-app/dist/loll-app.es.js
+++ packages/loll-app/dist/loll-app.es.js
@@ -269,16 +269,14 @@ function mitt(all                 ) {
 
 function href(cb) {
     window.addEventListener('click', function (e) {
-        if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.defaultPrevented) {
-            return;
-        }
+        if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.defaultPrevented) 
+            { return; }
         var a = e.target;
         while (a && !(a.href && a.nodeName === 'A')) {
             a = a.parentNode;
         }
-        if (!a || window.location.origin !== a.origin || a.hasAttribute('download') || a.target === '_blank' || /mailto|tel/.test(a.href)) {
-            return;
-        }
+        if (!a || window.location.origin !== a.origin || a.hasAttribute('download') || a.target === '_blank' || /mailto|tel/.test(a.href)) 
+            { return; }
         e.preventDefault();
         cb(a);
     });
